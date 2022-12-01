051fd0f0de9ce293979c0236e873514534796383
Laura Andrade <lauraandrad05@gmail.com>
2022-12-01 14:08:34 -0300

    aula78 lista

---
+++ Aula78/Aula78/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Aula78
+{
+    class Aula78
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many employees will be registered?");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i +":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                               
+            }
+
+            Console.WriteLine("Enter the employee id that will have salary increase : ");
+            int idSearch = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == idSearch);
+            
+            if(emp != null )
+            {
+                Console.WriteLine("Enter the percentage: ");
+                double percentege = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.increaseSalary(percentege);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+           
+
+
+        }
+    }
+}
